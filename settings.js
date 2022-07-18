83604b203173d14ca8a81300128fedb7231b7f48
fdhfgth <109281192+fdhfgth@users.noreply.github.com>
2022-07-18 21:32:31 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['+94 71 559 9312']
+global.premium = ['+94 71 559 9312']
+global.ownernomer = '+94 71 559 9312'
+global.ownername = '🐦Malindu Bot'
+global.botname = 'Maliya Bot'
+global.footer = '©Malindu.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Sri Lanka'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'https://youtube.com/channel/UCp0cfYXDnAqyqZsjNn8_xow'
+global.packname = 'Maliya Bot'
+global.author = '🐦🅼🅰🅻🅸🅽🅳🆄'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
